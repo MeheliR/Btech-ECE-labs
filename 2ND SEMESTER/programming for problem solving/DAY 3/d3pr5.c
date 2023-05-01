6181bb59f2607b382a734d9a2dac2292e54b0d90
MeheliR <meheli2022roy@gmail.com>
2023-05-01 16:35:41 +0530

    programming for problem solving DAY 3 d3pr5

---
+++ 2ND SEMESTER/programming for problem solving/DAY 3/d3pr5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main();
+{
+
+ int num1, num2,r,i;
+ printf("Enter the first number for the range: ");
+ scanf("%d",&num1); 
+ printf("Enter the second number for the range: ");
+ scanf("%d",&num2);
+ printf("\nDisplay the even numbers between %d and %d are: ",num1,num2);
+ for(i=num1; i<=num2; i++){
+ r=i%2;
+ if(r==0)
+ printf("\n %d",i);
+ }
+ printf("\n\nDisplay the odd numbers between %d and %d are: ",num1,num2);
+ for(i=num1; i<=num2; i++){
+ r=i%2;
+ if(r==1)
+ printf("\n %d",i);
+ }
+ 
+ return 0;
+ }
+ 
