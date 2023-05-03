bf898e0135454e2cbfb6e545d2e06926bb346d34
MeheliR <meheli2022roy@gmail.com>
2023-05-03 16:40:01 +0530

    programming for problem solving DAY 4 d4p2

---
+++ 2ND SEMESTER/programming for problem solving/DAY 4/d4p2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main () 
+{
+
+
+int n,a,b,c,x,d;
+
+a=0;
+b=1;
+c=0;
+printf("Enter the upper range of the series:");
+scanf("%d",&n);
+
+while(c<=n)
+{
+c=a+b;
+a=b;
+b=c;
+d=a+b;
+
+for(x=c+1;x<d;x++)
+{
+if(x<=n)
+printf("%d",x);
+else
+break;}
+}
+}
+
