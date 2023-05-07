60477a0b0bc171c72a0e483352523ee25f75029c
MeheliR <meheli2022roy@gmail.com>
2023-05-07 17:05:29 +0530

    programming for problem solving DAY 6 d6pr3&5

---
+++ 2ND SEMESTER/programming for problem solving/DAY 6/d6pr3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+Int main()
+
+{
+
+  Int I,j,k;
+
+  K=1;
+
+  For(i=1;i<5;i++)
+
+  {
+
+    For(j=1;j<=I;j++)
+
+    {
+
+      Printf("%d",k+=5);
+
+    }
+
+    Printf("\n");
+
+  }
+
+  Return 0;
+}
